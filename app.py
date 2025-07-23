ddc5b2cf2128a348f6b2431bd6d30142f3d5db3f
WarmupHero <c.antoniadis@acg.edu>
2025-07-23 20:26:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ def create_sentences_rag():
             st.session_state['my_sentences_rag_ids'] = [[i] for i in range(len(st.session_state['my_sentences']))]
 
             if len(st.session_state["word_embeddings"]) != len(st.session_state['my_sentences_rag']):
-                st.error(f"Mismatch: {len(st.session_state['my_sentences_rag'])} chunks vs {len(word_embeddings)} embeddings. Please fix this.")
+                st.error(f"Mismatch: {len(st.session_state['my_sentences_rag'])} chunks vs {len(st.session_state['word_embeddings'])} embeddings. Please fix this.")
                 st.stop()
             st.session_state['my_embeddings'] = word_embeddings
         st.success(f"{len(st.session_state['my_sentences_rag'])} chunks loaded successfully.")
